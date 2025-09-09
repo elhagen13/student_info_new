f516d5ee027958e5298c45f130c04aba1ee1f407
Ella Hagen <elhagen@calpoly.edu>
2025-09-09 16:23:47 -0700

    changed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // The `serverExternalPackages` option allows you to opt-out of bundling dependencies in your Server Components.
+    serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  };
+  
+  export default nextConfig;
+  
